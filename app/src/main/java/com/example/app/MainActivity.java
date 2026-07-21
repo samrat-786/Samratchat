e651c326b07397ae21d4afaf1eb78465b9b6a77d
samrat-786 <yourgmail@gmail.com>
2026-07-21 15:02:43 +0530

    Enable camera microphone and upload

--- app/src/main/java/com/example/app/MainActivity.java
+++ app/src/main/java/com/example/app/MainActivity.java
@@ -2,12 +2,9 @@ package com.example.app;
 
 import android.app.Activity;
 import android.os.Bundle;
-import android.view.Menu;
-import android.view.MenuItem;
 import android.webkit.WebSettings;
 import android.webkit.WebView;
 import android.webkit.WebViewClient;
-
 
 public class MainActivity extends Activity {
 
@@ -20,52 +17,28 @@ public class MainActivity extends Activity {
 
         mWebView = (WebView) findViewById(R.id.activity_main_webview);
 
-        // Force links and redirects to open in the WebView instead of in a browser
-        mWebView.setWebViewClient(new WebViewClient());
-
-        // Enable Javascript
         WebSettings webSettings = mWebView.getSettings();
         webSettings.setJavaScriptEnabled(true);
+        webSettings.setDomStorageEnabled(true);
+        webSettings.setAllowFileAccess(true);
+        webSettings.setAllowContentAccess(true);
+        webSettings.setDatabaseEnabled(true);
+        webSettings.setLoadsImagesAutomatically(true);
+        webSettings.setJavaScriptCanOpenWindowsAutomatically(true);
+        webSettings.setBuiltInZoomControls(false);
+        webSettings.setDisplayZoomControls(false);
 
-        // Use remote resource
-       mWebView.loadUrl("https://samrat-chat.onrender.com");
+        mWebView.setWebViewClient(new WebViewClient());
 
-        // Stop local links and redirects from opening in browser instead of WebView
-        mWebView.setWebViewClient(new MyAppWebViewClient());
-
-        // Use local resource
-        // mWebView.loadUrl("file:///android_asset/www/index.html");
+        mWebView.loadUrl("https://samrat-chat.onrender.com");
     }
 
-    // Prevent the back-button from closing the app
     @Override
     public void onBackPressed() {
-        if(mWebView.canGoBack()) {
+        if (mWebView.canGoBack()) {
             mWebView.goBack();
         } else {
             super.onBackPressed();
         }
-    }
-
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.menu_main, menu);
-        return true;
-    }
-
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
-
-        //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
-            return true;
-        }
-
-        return super.onOptionsItemSelected(item);
     }
 }
